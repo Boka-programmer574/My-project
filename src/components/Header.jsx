1e9148c1fe0e8bf585e479406298272bd1ea016c
Bokaprogrammer <Bokaprogram479@gmail.com>
2021-02-04 17:40:23 +0200

    change file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
-    return (
-        <header className='foto'>
-          
-        <img id='image' src='https://www.resonate.com/wp-content/uploads/2015/09/Volkswagen-Logo.jpg'></img>
-        <div className='strange'>Volksvagen group</div>
-      </header>
-    )
+    return <header className='foto'>
+        <img src="https://cdn.motor1.com/images/mgl/49L7M/s1/logo-story-volkswagen.jpg"/>
+    </header>
+        
+        
+        
+    
 }
 
 export default Header;
